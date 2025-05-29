138e08f7405b2ed640d3d7774881e94c234a6bfd
Shreyas M S <shreyasmadappady20@gmail.com>
2025-05-29 15:15:03 +0530

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: "Savory Touch",
+    default: "WordofMany",
     template: "Blog Upload",
   },
 };
