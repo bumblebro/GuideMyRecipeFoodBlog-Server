45df716d4f006cfc3af07be1e83f3c36aa0467f4
Shreyas M S <shreyasmadappady20@gmail.com>
2025-08-30 10:21:43 +0530

    c

--- prisma/migrations/20250830043952_init/migration.sql
+++ prisma/migrations/20250830043952_init/migration.sql
@@ -28,3 +28,15 @@ CREATE UNIQUE INDEX "FoodBlogs_title_key" ON "FoodBlogs"("title");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "FoodBlogs_slug_key" ON "FoodBlogs"("slug");
+
+-- CreateIndex
+CREATE INDEX "FoodBlogs_section_idx" ON "FoodBlogs"("section");
+
+-- CreateIndex
+CREATE INDEX "FoodBlogs_subsection_idx" ON "FoodBlogs"("subsection");
+
+-- CreateIndex
+CREATE INDEX "FoodBlogs_subsubsection_idx" ON "FoodBlogs"("subsubsection");
+
+-- CreateIndex
+CREATE INDEX "FoodBlogs_creationDate_idx" ON "FoodBlogs"("creationDate");
